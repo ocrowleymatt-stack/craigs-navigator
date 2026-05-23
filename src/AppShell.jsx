ecb9bf9c9d61d273ac81f07365393ea0c2bc7cd5
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:27:28 +0100

    Add polished dashboard shell

---
+++ src/AppShell.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import HeroHeader from './components/HeroHeader';
+import RealityMonitor from './components/RealityMonitor';
+import MorningReport from './components/MorningReport';
+import NotificationCentre from './components/NotificationCentre';
+import CompanionPanel from './components/CompanionPanel';
+import CompanionSharePanel from './components/CompanionSharePanel';
+
+export default function AppShell(){
+ return <main className='app-shell'>
+ <HeroHeader/>
+ <div className='grid'>
+ <MorningReport overnightEvents={[]}/>
+ <NotificationCentre/>
+ <CompanionPanel/>
+ <CompanionSharePanel/>
+ </div>
+ <RealityMonitor/>
+ </main>;
+}
