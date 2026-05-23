bf7954fcdf89ca97fdca76a816bee2af9f19d82e
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:28:41 +0100

    Add premium metric strip to shell

--- src/AppShell.jsx
+++ src/AppShell.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HeroHeader from './components/HeroHeader';
+import MetricStrip from './components/MetricStrip';
 import RealityMonitor from './components/RealityMonitor';
 import MorningReport from './components/MorningReport';
 import NotificationCentre from './components/NotificationCentre';
@@ -9,6 +10,7 @@ import CompanionSharePanel from './components/CompanionSharePanel';
 export default function AppShell(){
  return <main className='app-shell'>
  <HeroHeader/>
+ <MetricStrip/>
  <div className='grid'>
  <MorningReport overnightEvents={[]}/>
  <NotificationCentre/>
