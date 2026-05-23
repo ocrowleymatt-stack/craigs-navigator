f8314dad3bfcf61b0bc8cc7485b3a6d4a7b23eae
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:27:11 +0100

    Add polished hero header component

---
+++ src/components/HeroHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HeroHeader(){
+ return <header className="hero">
+  <div className="badge">Navigator Alpha</div>
+  <h1>Craig's Navigator</h1>
+  <p className="subtitle">Quietly helping things make sense.</p>
+ </header>;
+}
