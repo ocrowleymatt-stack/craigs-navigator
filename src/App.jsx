4c904a69f7fdc261e8ae2f548ae6bedacc1a8d5b
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:20:24 +0100

    Add Morning Report to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './styles/app.css';
 import RealityMonitor from './components/RealityMonitor';
+import MorningReport from './components/MorningReport';
+
 createRoot(document.getElementById('root')).render(
 <main className='app-shell'>
 <h1>Craig\'s Navigator</h1>
-<p>Reality checking, calm and acoustic monitoring.</p>
+<p>Quietly helping things make sense.</p>
+<MorningReport overnightEvents={[]}/>
 <RealityMonitor/>
 </main>
 );
