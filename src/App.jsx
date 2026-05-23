1d527f4844dfb44c9f4a6a88e3d28786d364a1da
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:21:25 +0100

    Add notification centre to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {createRoot} from 'react-dom/client';
 import './styles/app.css';
 import RealityMonitor from './components/RealityMonitor';
 import MorningReport from './components/MorningReport';
+import NotificationCentre from './components/NotificationCentre';
 
 createRoot(document.getElementById('root')).render(
 <main className='app-shell'>
 <h1>Craig\'s Navigator</h1>
 <p>Quietly helping things make sense.</p>
 <MorningReport overnightEvents={[]}/>
+<NotificationCentre/>
 <RealityMonitor/>
 </main>
 );
