e5cb147c384759f57ca2db68b8831781bfc478b0
ocrowleymatt-stack <ocrowley.matt@gmail.com>
2026-05-23 14:27:41 +0100

    Add stable entrypoint for polished shell

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './styles/app.css';
+import AppShell from './AppShell';
+
+createRoot(document.getElementById('root')).render(<AppShell/>);
